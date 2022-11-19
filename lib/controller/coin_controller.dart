eebfedac163bfa70e83680c4db49c891784e9d1b
AdarshHota <adarshhota55@gmail.com>
2022-11-19 10:47:18 +0530

    refactor: move API key to utils file

--- lib/controller/coin_controller.dart
+++ lib/controller/coin_controller.dart
@@ -1,4 +1,5 @@
 import 'package:crypto_list_app/modals/coin_modal.dart';
+import 'package:crypto_list_app/utils.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
@@ -15,8 +16,9 @@ class CoinController extends GetxController {
   Future<void> fetchCoins() async {
     try {
       isLoading(true);
-      var response = await http.get(Uri.parse(
-          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'));
+      var response = await http.get(
+        Uri.parse(apiEndpointUrl),
+      );
       coinsList.value = coinModalFromJson(response.body);
     } finally {
       isLoading(false);
--- lib/utils.dart
+++ lib/utils.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+const String apiEndpointUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+
 const Map<String, String> dropDownMenu = {
   'weight': 'Weight',
   'height': 'Height',
